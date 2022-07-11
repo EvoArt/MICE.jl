3ec7e08047bcc8e2dab1ee6d01fd033d162d1702
Documenter.jl <documenter@juliadocs.github.io>
2022-07-11 21:28:29 +0000

    build based on ee6c887

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MICE","category":"page"},{"location":"#MICE","page":"Home","title":"MICE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MICE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MICE]","category":"page"}]
+}
